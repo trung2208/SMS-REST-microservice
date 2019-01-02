39c5d870b205865789fc8d2719ab7bd070a33c83
Trung-PC <Trung-PC@Trung-PC>
2019-01-02 21:35:39 +0700

    Up to date 1

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 
 var express = require('express'),
         app = express(),
-        port = process.env.PORT || 800;
+        port = process.env.PORT || 3000;
 bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
